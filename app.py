947667c37cfac06d2fc23d835b3a27cbfc71e91b
yadegenn <yadegenn@gmail.com>
2026-02-08 13:31:40 +0500

    create base files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from db import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    return app
+
+create_app()
